4deb3c02ec13822e95b9727063b258ecdffeb0c1
Alexander Shipunov <beshemel@gmail.com>
2016-12-02 23:31:27 +0100

    add time stamp and move format to logging

--- logging.c
+++ logging.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <time.h>
+#include <ctype.h>
 
 void startGameLog(char* black, char* white) {
+
+	char stringTime[100];
+
+	time_t t = time(NULL);
+	struct tm * p = localtime(&t);
+
+	strftime(stringTime, 1000, "%X %A, %B %d %Y", p);
 	
 	FILE *log = fopen("logfile.txt", "at");
     if (!log) 
@@ -11,28 +20,41 @@ void startGameLog(char* black, char* white) {
         return;   
     }
 	
-	fprintf(log, "\n===========================================\n");
-	fprintf(log, "Match between %s(BLACK) and %s(WHITE)", black, white);
-	fprintf(log, "\n===========================================\n");
+	fprintf(log, "\n===================================================================\n");
+	fprintf(log, "Match between %s(BLACK) and %s(WHITE) /started at %s/", 
+							black, white,  stringTime);
+	fprintf(log, "\n===================================================================\n");
 	
 	fclose(log);
 }
 
 void saveMoveToLog(bool blackMove, char* black, char* white, 
 							int rowIndex, int columnIndex, bool wrongMove){
+							
+	char stringTime[100];
+
+	time_t t = time(NULL);
+	struct tm * p = localtime(&t);
+
+	strftime(stringTime, 1000, "%X %A, %B %d %Y", p);
+	
 	FILE *log = fopen("logfile.txt", "at");
-    if (!log) log = fopen("logfile.txt", "wt");
+    if (!log) 
+    	log = fopen("logfile.txt", "wt");
     if (!log) {
         printf("can not open logfile.txt for writing.\n");
         return;   
     }
     
+    
     if(blackMove) {
-    	fprintf(log, "%s made move: %d-%d, the move was %s\n", black, 
-    				rowIndex, columnIndex, wrongMove ? "wrong" : "correct");
+    	fprintf(log, "%s made move: %d-%c at %s, the move was %s.\n", black, 
+    	rowIndex + 1, columnIndex + 65, stringTime, 
+    										wrongMove ? "wrong" : "correct");
     } else {
-    	fprintf(log, "%s made move: %d-%d, the move was %s\n", white, 
-    				rowIndex, columnIndex, wrongMove ? "wrong" : "correct");
+    	fprintf(log, "%s made move: %d-%c at %s, the move was %s.\n", white, 
+    	rowIndex + 1, columnIndex + 65, stringTime, 
+    										wrongMove ? "wrong" : "correct");
     }
 
     fclose(log);
